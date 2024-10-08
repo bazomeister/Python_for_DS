03c90956f4495e30ab26e07b1a0225f60b336380
bazomeister <106102981+bazomeister@users.noreply.github.com>
2024-10-08 14:55:04 +0200

    Delete ex_2.2.py

--- ex_2.2.py
+++
@@ -1,6 +0,0 @@
-# a list with integers, strings, and floats
-mixed_data = [1, "esade", 3.14, 92, "Barcelona", 2.718]
-
-# create a new list with only integers from the original list
-integers = [item for item in mixed_data if isinstance(item, int)]
-print(integers)
