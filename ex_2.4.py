ff550815bd66446a1357dbdb97ce5978bc73109e
bazomeister <106102981+bazomeister@users.noreply.github.com>
2024-10-08 14:55:14 +0200

    Delete ex_2.4.py

--- ex_2.4.py
+++
@@ -1,8 +0,0 @@
-#Defining function for tempretur conversion
-def celsius_to_fahrenheit(celsius):
-    return (celsius * 9/5) + 32
-
-# Test function with given temperatures using a loop
-temperatures_celsius = [22, 46, 51, 76]
-for temp in temperatures_celsius:
-    print(f"{temp}C is {celsius_to_fahrenheit(temp)}F")
